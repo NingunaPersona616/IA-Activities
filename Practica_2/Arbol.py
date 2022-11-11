5fc7fef7fe2e28ce7d8008e93020fefd661670a4
Brandon Hernandez Ledezma <brandon.hernandez5150@alumnos.udg.mx>
2022-11-10 23:15:05 -0600

    Deep First Search working with some examples

--- Practica_2/Arbol.py
+++ Practica_2/Arbol.py
@@ -1,15 +1,21 @@
 from Nodo import *
 
+initialState = np.array([[1,2,5],[3,4,8],[6,7,0]])
+
 class Tree:
     def __init__(self):
-        initialState = [[3,1,2],[6,4,5],[0,7,8]]
         self.root = Node()
         self.root.setSolucion(initialState)
+        self.nodesList=[]
+        self.nodesList.append(self.root.Data)
+        self.path=[]
 
     def deepSearch(self):
-        nodesList=[]
-        nodesList.append(self.root.Data)
-        
-        result = self.root.expandNodes(nodesList,0)
+        result = self.root.expandNodes(self.nodesList,0, self.path)
         if isSolution(result):
             print("se encontro la solucion")
+
+            print(target, '\n')
+            for node in self.path:
+                print(node, "\n")
+            print(initialState)
--- Practica_2/Nodo.py
+++ Practica_2/Nodo.py
@@ -1,21 +1,23 @@
+import numpy as np
+
 Upper_X=3
 Upper_Y=3
 maxLevel = 10
-target = [[0,1,2],[3,4,5],[6,7,8]] #Estado objetivo
+target = np.array([[0,1,2],[3,4,5],[6,7,8]]) #Estado objetivo
 
 def copy(data):
     pass
 
 def isSolution(n):
-    if n == None:
+    if n is None:
         return False
-    elif n == target:
+    elif (n == target).all():
         return True
 
 class Node:
     def __init__(self):
         self.Nodes = []
-        self.Data = None
+        self.Data = np.empty(0)
         self.x_Pos = None
         self.y_Pos = None
 
@@ -25,6 +27,11 @@ class Node:
         self.left = True
         self.right = True
 
+    def isOpened(self, newData, openNodes):
+        for node in openNodes:
+            if (newData == node).all():
+                return True
+        return False
 
     def setSolucion(self, Solution):    #Considerar para el laberinto no buscar sino pasar como parametros las coordenadas
         self.Data = Solution
@@ -32,12 +39,12 @@ class Node:
         for i in range(Upper_X):    #Busca donde se encuentra el espacio en blanco
             for j in range(Upper_Y):
                 if self.Data[i][j] == 0:
-                    
                     self.x_Pos = i
                     self.y_Pos = j
                     break
         
-        print("El 0 esta en:", self.x_Pos, self.y_Pos)
+        print("Abriendo Nodo")
+        print(Solution, "\n")
 
         if self.x_Pos == 0: #si esta en el primer renglon no puede moverse hacia arriba
             self.up = False
@@ -52,129 +59,128 @@ class Node:
             self.right = False
 
     
-    def expandNodes(self, openNodes, level):
+    def expandNodes(self, openNodes, level, path):
         if level == maxLevel:
             return None
 
         #Si se puede mover hacia arriba
         if self.up == True:
-            newData = copy(self.Data) #Copia de la data actual
+            newData = np.copy(self.Data) #Copia de la data actual
 
             aux = newData[self.x_Pos-1][self.y_Pos] #guarda el dato que esta arriba de el
             newData[self.x_Pos-1][self.y_Pos] = 0  #mueve el espacio en blanco hacia arriba
             newData[self.x_Pos][self.y_Pos] = aux  #mueve el de arriba hacia abajo
 
-            print(newData)
 
             if isSolution(newData):
+                print("Solucion")
+                print(newData, "\n")
                 return newData
 
             #Suponiendo que in hace comparaciones de matrices uno a uno
-            elif newData in openNodes == False:    #Si el nodo por abir no ha sido abierto
-                print("entro")
+            if(self.isOpened(newData, openNodes)) == False:    #Si el nodo por abir no ha sido abierto
                 newNode = Node()                
                 newNode.setSolucion(newData)    #Guarda el nuevo nodo
                 self.Nodes.append(newNode)
                 openNodes.append(newData)
 
-                result = self.Nodes[0].expandNodes(openNodes, level+1) #Expandimos el nuevo nodo creado y busca la sol en sus hijos
+                result = self.Nodes[0].expandNodes(openNodes, level+1, path) #Expandimos el nuevo nodo creado y busca la sol en sus hijos
 
                 if isSolution(result):
+                    path.append(newNode.Data)
                     return result
 
                 #Quitamos los nodos que no son solucion?
-                openNodes.pop(0)
-                self.Nodes.pop(0)
-
-        print("Up:", self.up, " Down:", self.down, " L:", self.left, " R:", self.right)
+                #openNodes.pop(0)
+                #self.Nodes.pop(0)
 
         #Si se puede mover hacia abajo
         if self.down == True:
-            newData = copy(self.Data) #Copia de la data actual
+            newData = np.copy(self.Data) #Copia de la data actual
 
             aux = newData[self.x_Pos+1][self.y_Pos] #guarda el dato que esta abajo de el
             newData[self.x_Pos+1][self.y_Pos] = 0  #mueve el espacio en blanco hacia abajo
             newData[self.x_Pos][self.y_Pos] = aux  #mueve el de abajo hacia arriba
 
             if isSolution(newData):
-                #Guardar en otra lista los nodos solucion?
+                print("Solucion")
+                print(newData, "\n")
                 return newData
 
-            elif newData in openNodes == False:    #Si el nodo por abir no ha sido abierto
+            if(self.isOpened(newData, openNodes)) == False:     #Si el nodo por abir no ha sido abierto
                 newNode = Node()                
                 newNode.setSolucion(newData)    #Guarda el nuevo nodo
                 self.Nodes.append(newNode)
                 openNodes.append(newData)
 
-                result = self.Nodes[0].expandNodes(openNodes, level+1) #Expandimos el nuevo nodo creado y busca la sol en sus hijos
+                result = self.Nodes[0].expandNodes(openNodes, level+1, path) #Expandimos el nuevo nodo creado y busca la sol en sus hijos
 
                 if isSolution(result):
-                    #Guardar en otra lista los nodos solucion?
+                    path.append(newNode.Data)
                     return result
 
                 #Quitamos los nodos que no son solucion?
-                openNodes.pop(0)
-                self.Nodes.pop(0)
+                #openNodes.pop(0)
+                #self.Nodes.pop(0)
 
         #Si se puede mover hacia la izquierda
         if self.left == True:
-            newData = copy(self.Data) #Copia de la data actual
+            newData = np.copy(self.Data) #Copia de la data actual
 
             aux = newData[self.x_Pos][self.y_Pos-1] #guarda el dato que esta a su izquierda
             newData[self.x_Pos][self.y_Pos-1] = 0   #mueve el espacio en blanco hacia la izquierda
             newData[self.x_Pos][self.y_Pos] = aux   #mueve el dato hacia la derecha
 
             if isSolution(newData):
-                #Guardar en otra lista los nodos solucion?
+                print("Solucion")
+                print(newData, "\n")
                 return newData
 
-            elif newData in openNodes == False:    #Si el nodo por abir no ha sido abierto
+            if(self.isOpened(newData, openNodes)) == False:    #Si el nodo por abir no ha sido abierto
                 newNode = Node()                
                 newNode.setSolucion(newData)    #Guarda el nuevo nodo
                 self.Nodes.append(newNode)
                 openNodes.append(newData)
 
-                result = self.Nodes[0].expandNodes(openNodes, level+1) #Expandimos el nuevo nodo creado y busca la sol en sus hijos
+                result = self.Nodes[0].expandNodes(openNodes, level+1, path) #Expandimos el nuevo nodo creado y busca la sol en sus hijos
 
                 if isSolution(result):
-                    #Guardar en otra lista los nodos solucion?
+                    path.append(newNode.Data)
                     return result
 
                 #Quitamos los nodos que no son solucion?
-                openNodes.pop(0)
-                self.Nodes.pop(0)
+                #openNodes.pop(0)
+                #self.Nodes.pop(0)
         
-        print("Up:", self.up, " Down:", self.down, " L:", self.left, " R:", self.right)
         #Si se pude mover a la derecha
         if self.right == True:
-            newData = copy(self.Data) #Copia de la data actual
+            newData = np.copy(self.Data) #Copia de la data actual
             
             aux = newData[self.x_Pos][self.y_Pos+1] #Copia el dato de su derecha
             newData[self.x_Pos][self.y_Pos+1] = 0     #Mueve el espacio en blanco a la derecha
             newData[self.x_Pos][self.y_Pos] = aux  #Mueve el otro dato a la izquiera
 
-            print(newData)
-
             if isSolution(newData):
-                #Guardar en otra lista los nodos solucion?
+                print("Solucion")
+                print(newData, "\n")
                 return newData
 
             #Suponiendo que in hace comparaciones de matrices uno a uno
-            elif newData in openNodes == False:    #Si el nodo por abir no ha sido abierto
+            if(self.isOpened(newData, openNodes)) == False:    #Si el nodo por abir no ha sido abierto
                 newNode = Node()                
                 newNode.setSolucion(newData)    #Guarda el nuevo nodo
                 self.Nodes.append(newNode)
                 openNodes.append(newData)
 
-                result = self.Nodes[0].expandNodes(openNodes, level+1) #Expandimos el nuevo nodo creado y busca la sol en sus hijos
+                result = self.Nodes[0].expandNodes(openNodes, level+1, path) #Expandimos el nuevo nodo creado y busca la sol en sus hijos
 
                 if isSolution(result):
-                    #Guardar en otra lista los nodos solucion?
+                    path.append(newNode.Data)
                     return result
 
                 #Quitamos los nodos que no son solucion?
-                openNodes.pop(0)
-                self.Nodes.pop(0)
+                #openNodes.pop(0)
+                #self.Nodes.pop(0)
         
         return None #Si ningun nodo es solucion regresa []
 
