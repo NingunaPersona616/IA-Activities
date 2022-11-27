419d6eb3146b24c6058f0f931392e98bf43e43fa
Brandon Hernandez Ledezma <brandon.hernandez5150@alumnos.udg.mx>
2022-11-26 23:29:21 -0600

    Deleted unnecesary stuff

--- Laberinto_Amplitud/Main.py
+++
@@ -1,7 +0,0 @@
-from Nodo import *
-
-if __name__ == '__main__':                                         #Función principal.
-    #Leemos el laberinto del archivo de texto para cargarlo a memoria.
-    print('\nL A B E R I N T O   Amplitud \n')
-    laberinto.dibujar_muros()   
-    print(acciones(laberinto.inicio, estados_explorados))
