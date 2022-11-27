d80e1f6b40cfc4c23d326623c106faa741f0a998
Brandon Hernandez Ledezma <brandon.hernandez5150@alumnos.udg.mx>
2022-11-26 23:19:00 -0600

    maze DFS added

--- Laberinto_Profundidad/Main.py
+++ Laberinto_Profundidad/Main.py
@@ -1,7 +1,29 @@
-from Nodo import *
+from DFS import *
 
-if __name__ == '__main__':                                         #Función principal.
-    #Leemos el laberinto del archivo de texto para cargarlo a memoria.
-    print('\nL A B E R I N T O   Amplitud \n')
-    laberinto.dibujar_muros()   
-    print(acciones(laberinto.inicio, estados_explorados))
+
+if __name__ == '__main__': 
+    print("Laberinto inicial:")
+    laberinto.dibujar_muros()  
+    print("Inicio:", laberinto.inicio)
+    print("Meta:", laberinto.meta)
+    print()
+    print("\nLaberinto resuelto:")
+    solution = DFS_Interface(laberinto.inicio, estados_explorados)
+
+    if solution is not None:
+        ruta.append(solution.estado)
+        Aux = solution.nodoPadre
+        while True:
+            if Aux.nodoPadre == None:
+                ruta.append(Aux.estado)
+                break
+            else:
+                ruta.append(Aux.estado)
+                Aux = Aux.nodoPadre
+    
+        laberinto.dibujar_solucion(ruta, estados_explorados)
+        print("\nLa ruta que recorrio fue:\n")
+        print(ruta[::-1])
+    else:
+        print("El laberinto fallo y realizo la busqueda en todo esto:")
+        laberinto.dibujar_falla(estados_explorados)
